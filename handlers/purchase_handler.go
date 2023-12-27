1240048c437f97f37e17eb52708bb8f3c626341f
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-27 10:59:06 +0700

    Added a starting code for PurchaseOrder()

--- handlers/purchase_handler.go
+++ handlers/purchase_handler.go
@@ -19,6 +19,73 @@ func PurchaseOrder(c *gin.Context) {
 			}))
 	}
 
+	// validate input data
+	err = ValidatePurchaseOrderData(&purchaseOrder)
+	if err != nil {
+		c.JSON(http.StatusBadRequest,
+			responses.PurchaseErrorResponse([]string{
+				err.Error(),
+			}))
+	}
+
+	// check user existence
+	user, err := GetUserByID(purchaseOrder.UserID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest,
+			responses.PurchaseErrorResponse([]string{
+				"Failed to retrieve user",
+				err.Error(),
+			}))
+	}
+
+	// check product existence and available stock + calculate subtotal
+	var subtotal float64
+	for _, product := range purchaseOrder.ProductsID {
+
+		// find if product exist
+		p, err := GetProductByID(product.ProductID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest,
+				responses.PurchaseErrorResponse([]string{
+					"Failed to retrieve product",
+					err.Error(),
+				}))
+		}
+
+		// compare purchase quantity and available stock
+		if product.quantity > p.Stock {
+			c.JSON(http.StatusBadRequest,
+				responses.PurchaseErrorResponse([]string{
+					"Insufficient stock for ProductID: " + string(product.ProductID),
+					err.Error(),
+				}))
+			return
+		}
+
+		subtotal += float64(product.quantity) * p.Price
+
+		UpdateStock(product.ProductID, product.Quantity)
+	}
+	totalAmount := subtotal
+
+	// create order
+	orderID, err := CreateOrder(purchaseOrder.UserID, totalAmount)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError,
+			responses.PurchaseErrorResponse([]string{
+				"Failed to create order",
+				err.Error(),
+			}))
+		return
+	}
+
+	// create order detail
+	for _, product := range purchaseOrder.Products {
+		productInfo, _ := getProductByID(product.ProductID)
+		subtotal := float64(product.Quantity) * productInfo.Price
+		CreateOrderDetail(orderID.gorm.Model.ID, product.ProductID, product.Quantity, subtotal)
+	}
+
 	// * dont for get to do transaction
 	// validate input data
 	// check user, product exist
--- models/purchase.go
+++ models/purchase.go
@@ -2,6 +2,6 @@ package models
 
 type PurchaseOrder struct {
 	UserID      uint      `json:"userId" binding:"required"`
-	ProductsID  []Product `json:"products" binding:"required"`
+	Products    []Product `json:"products" binding:"required"`
 	TotalAmount float64   `json:"totalAmount" binding:"required,gte=1"`
 }
